8a2eb3c46159f207450328739c8fb77a2299497b
yuyuanyuana <53144397+yuyuanyuana@users.noreply.github.com>
2022-09-20 09:47:00 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SECE',  
+    packages=find_packages(),
+    version='1.0.0',
+    description='Spatial region-related embedding and Cell type-related embedding of spatial transcriptomics',
+    author='yuanyuanyu',
+    license='MIT',
+    URL='https://github.com/yuyuanyuana/SECE'
+)
